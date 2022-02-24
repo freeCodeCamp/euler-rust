a311becfe9f892a8418e02c225d7183745c0344c
Shaun Hamilton <shauhami020@gmail.com>
2022-02-24 18:13:07 +0000

    fix what I can, ignore what I must

--- .freeCodeCamp/tooling/t.js
+++ .freeCodeCamp/tooling/t.js
@@ -1,12 +1,12 @@
-const { readEnv } = require("./env");
+const { readEnv } = require('./env');
 
-const LOCALE = readEnv(".meta").LOCALE;
+const LOCALE = readEnv('.meta').LOCALE;
 
 function t(key, args = {}, forceLangToUse) {
   const loc = readEnv().LOCALE;
   // Get key from ./locales/{locale}/comments.json
   // Read file and parse JSON
-  const locale = forceLangToUse ?? loc === "undefined" ? "english" : loc;
+  const locale = forceLangToUse ?? loc === 'undefined' ? 'english' : loc;
   const comments = require(`./locales/${locale}/comments.json`);
 
   // Get value from JSON
